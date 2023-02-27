e2124290491425a16ac1dac6f321fbc0e33c03ec
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-02-28 12:26:59 +1300

    Upload Files Via GitHub Desktop

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Generated with DurtyClothTool
+-- Pack created for free by TayMcKenzieNZ
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_merchpack_taymckenzienz.meta',
+  'mp_f_freemode_01_mp_f_merchpack_taymckenzienz.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_merchpack_taymckenzienz.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_merchpack_taymckenzienz.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
